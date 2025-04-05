775b3c2b687e74eba20bc2cf19bf2c26df737fe4
DESKTOP-RN53QV1\Euan <javata@nate.com>
2025-04-05 22:09:46 +0900

    vagrant mariadb 설치

--- vagrant/ch1.3/vagrant-rocky-mariadb/create_vm.bat
+++ vagrant/ch1.3/vagrant-rocky-mariadb/create_vm.bat
@@ -1,2 +1,29 @@
 @echo off
-vagrant up
+chcp 65001 > nul
+setlocal
+
+:: VM 이름 정의
+set VM_NAME1=mariadb20
+set VM_NAME2=mariadb21
+set VM_NAME3=mariadb22
+
+:: VM PATH 정의
+set VM_PATH1="%USERPROFILE%\VirtualBox VMs\%VM_NAME1%"
+set VM_PATH2="%USERPROFILE%\VirtualBox VMs\%VM_NAME2%"
+set VM_PATH3="%USERPROFILE%\VirtualBox VMs\%VM_NAME3%"
+
+:: VirtualBox의 전체 경로로 VBoxManage 실행
+set VBOXMANAGE="C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
+
+:: VirtualBox에 등록된 VM인지 확인하고 생성
+for %%i in (%VM_NAME1% %VM_NAME2% %VM_NAME3%) do (
+    %VBOXMANAGE% list vms | findstr /i "%%i" >nul
+    if !errorlevel! == 0 (
+        echo VM %%i 가 VirtualBox에 존재합니다.
+    ) else (
+        echo VM %%i 를 생성합니다.
+        vagrant up %%i
+    )
+)
+
+endlocal
--- vagrant/ch1.3/vagrant-rocky-mariadb/remove_vm.bat
+++ vagrant/ch1.3/vagrant-rocky-mariadb/remove_vm.bat
@@ -15,14 +15,14 @@ set VM_PATH3="%USERPROFILE%\VirtualBox VMs\%VM_NAME3%"
 :: VirtualBox의 전체 경로로 VBoxManage 실행
 set VBOXMANAGE="C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
 
-:: Vagrant VM 상태 확인
+:: VirtualBox에 등록된 VM인지 확인하고 삭제
 for %%i in (%VM_NAME1% %VM_NAME2% %VM_NAME3%) do (
-    vagrant status %%i >nul 2>&1
-    if %errorlevel% equ 0 (
-        echo VM %%i 가 존재합니다. 삭제를 시작합니다.
+    %VBOXMANAGE% list vms | findstr /i "%%i" >nul
+    if !errorlevel! == 0 (
+        echo VM %%i 가 VirtualBox에 존재합니다. 삭제를 시작합니다.
         vagrant destroy %%i -f
     ) else (
-        echo VM %%i 는 존재하지 않습니다.
+        echo VM %%i 는 VirtualBox에 존재하지 않습니다.
     )
 )
 
