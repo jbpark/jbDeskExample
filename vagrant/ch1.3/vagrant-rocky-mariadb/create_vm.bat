a7a9bbd1d4ec9da6a3273c6fd5785fe983903fae
DESKTOP-RN53QV1\Euan <javata@nate.com>
2025-04-05 21:26:04 +0900

    vagrant mariadb

--- vagrant/ch1.3/vagrant-rocky-mariadb/create_vm.bat
+++ vagrant/ch1.3/vagrant-rocky-mariadb/create_vm.bat
@@ -1,23 +1,2 @@
 @echo off
-set VM_NAME=oracle21c
-set VM_PATH=%USERPROFILE%\VirtualBox VMs\%VM_NAME%
-set VDI_FILE=%VM_NAME%.vdi
-
-:: VirtualBox의 전체 경로로 VBoxManage 실행
-set VBOXMANAGE="C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
-
-if exist "%VDI_FILE%" (
-    %VBOXMANAGE% closemedium disk "%VDI_FILE%"
-    del /f /q "%VDI_FILE%"
-    echo %VDI_FILE% has been deleted.
-) else (
-    echo %VDI_FILE% does not exist.
-)
-
-if exist "%VM_PATH%" (
-    echo %VM_NAME% already exists.
-) else (
-    echo %VM_NAME% does not exist. Starting Vagrant VM...
-    vagrant halt
-    vagrant up
-)
+vagrant up
